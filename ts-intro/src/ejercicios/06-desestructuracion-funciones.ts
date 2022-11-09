87dda4d1b57df32d6af4622313dbc6c15ae9ba82
rodrigopla97 <rodrigoplaceres19@gmail.com>
2022-11-09 17:13:23 -0300

    Desestructuración de argumentos (Funciones)

---
+++ ts-intro/src/ejercicios/06-desestructuracion-funciones.ts
@@ -0,0 +1,32 @@
+// Codigo TypeScript
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    desc: 'Nokia A1',
+    precio: 150
+}
+
+const tableta: Producto = {
+    desc: 'Ipad Air',
+    precio: 350
+}
+
+function calcularISV(productos: Producto[]): [number, number] {
+
+    let total = 0;
+
+    productos.forEach(({ precio }) => {
+        total += precio;
+    })
+    return [total, total * 0.15];
+}
+
+const articulos = [telefono, tableta]
+const [total, isv] = calcularISV(articulos);
+
+console.log('Total:', total);
+console.log('ISV:', isv);
